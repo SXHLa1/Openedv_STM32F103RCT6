82209e582c0408daf7bf34242486f36da150e5c4
dehongyi <dehongyihh@outlook.com>
2023-12-04 23:50:13 +0800

    1.调试跑马灯
    Signed-off-by: dehongyi <dehongyihh@outlook.com>

--- USER/drive/drv_led.c
+++ USER/drive/drv_led.c
@@ -29,9 +29,9 @@ void led_set()
 {
         GPIO_ResetBits(LED0_PORT,LED0_PIN);
         GPIO_SetBits(LED1_PORT,LED1_PIN);
-        delay_ms(300);
+        delay_ms(3000);
         GPIO_SetBits(LED0_PORT,LED0_PIN);
         GPIO_ResetBits(LED1_PORT,LED1_PIN);
-        delay_ms(300);
+        delay_ms(3000);
 }
 
